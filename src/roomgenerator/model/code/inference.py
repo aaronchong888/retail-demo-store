aeea480cb232ed788066e3887618ba9606152dd3
Aaron Chong <aachong@amazon.com>
2024-09-06 18:41:28 +0800

    Modified for auto mask generation and inpainting

--- src/roomgenerator/model/code/inference.py
+++ src/roomgenerator/model/code/inference.py
@@ -3,11 +3,13 @@ import json
 import base64
 import torch
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps, ImageFilter
 from transformers import DPTImageProcessor, DPTForDepthEstimation
-from diffusers import StableDiffusionXLControlNetPipeline, ControlNetModel, HeunDiscreteScheduler, AutoencoderKL
+from diffusers import StableDiffusionXLInpaintPipeline, StableDiffusionXLControlNetPipeline, StableDiffusionXLControlNetInpaintPipeline, ControlNetModel, HeunDiscreteScheduler, AutoencoderKL, AutoPipelineForInpainting
 from compel import Compel, ReturnedEmbeddingsType
 import boto3
+from rembg import remove 
+from controlnet_aux import ZoeDetector
 
 s3_client = boto3.client('s3')
 
@@ -16,28 +18,60 @@ def model_fn(model_dir):
     depth_estimator = DPTForDepthEstimation.from_pretrained(model_dir + "/dpt-hybrid-midas").to("cuda")
     feature_extractor = DPTImageProcessor.from_pretrained(model_dir + "/dpt-hybrid-midas")
     
-    controlnet = ControlNetModel.from_pretrained(
-        model_dir + "/controlnet-depth-sdxl-1.0",
-        variant="fp16",
-        use_safetensors=True,
-        torch_dtype=torch.float16,
-    ).to("cuda")
+    controlnet = [
+        ControlNetModel.from_pretrained(
+            model_dir + "/controlnet-inpaint-dreamer-sdxl", #controlnet-depth-sdxl-1.0
+            torch_dtype=torch.float16,
+            variant="fp16"
+        ),
+        ControlNetModel.from_pretrained(
+            model_dir + "/controlnet-zoe-depth-sdxl-1.0",
+            torch_dtype=torch.float16
+        ),
+    ]
+
+    #controlnet = ControlNetModel.from_pretrained(
+    #    model_dir + "/controlnet-depth-sdxl-1.0",
+    #    variant="fp16",
+    #    use_safetensors=True,
+    #    torch_dtype=torch.float16,
+    #).to("cuda")
     
     vae = AutoencoderKL.from_pretrained(
         model_dir + "/sdxl-vae-fp16-fix", 
         torch_dtype=torch.float16
     ).to("cuda")
     
+    # TEST @aachong
+    zoe = ZoeDetector.from_pretrained("lllyasviel/Annotators")
+    
+    #pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+    #    model_dir + "/stable-diffusion-xl-base-1.0",
+    #    controlnet=controlnet,
+    #    vae=vae,
+    #    variant="fp16",
+    #    use_safetensors=True,
+    #    torch_dtype=torch.float16,
+    #).to("cuda")
+    
     pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
-        model_dir + "/stable-diffusion-xl-base-1.0",
+        model_dir + "/RealVisXL_V4.0",
         controlnet=controlnet,
         vae=vae,
         variant="fp16",
         use_safetensors=True,
         torch_dtype=torch.float16,
     ).to("cuda")
+    
     # print(pipe.scheduler.compatibles)
     pipe.scheduler = HeunDiscreteScheduler.from_config(pipe.scheduler.config)
+    
+    #pipe2 = StableDiffusionXLInpaintPipeline.from_pretrained(
+    #    model_dir + "/RealVisXL_V4.0_inpainting",
+    #    torch_dtype=torch.float16,
+    #    variant="fp16",
+    #    vae=vae,
+    #).to("cuda")
     
     compel = Compel(
       tokenizer=[pipe.tokenizer, pipe.tokenizer_2] ,
@@ -46,7 +80,7 @@ def model_fn(model_dir):
       requires_pooled=[False, True]
     )
 
-    return depth_estimator, feature_extractor, controlnet, vae, pipe, compel
+    return depth_estimator, feature_extractor, controlnet, vae, pipe, compel, zoe #, pipe2
 
 def input_fn(request_body, request_content_type):    
     if request_content_type == 'application/json':
@@ -61,30 +95,76 @@ def input_fn(request_body, request_content_type):
 # Predict the result using the model
 def predict_fn(input_data, model):
     image, data = input_data
-    depth_estimator, feature_extractor, controlnet, vae, pipe, compel = model
+    image = image.resize((1024,1024)).convert('RGB')
+    depth_estimator, feature_extractor, controlnet, vae, pipe, compel, zoe = model #, pipe2
     prompt = data['prompt']
     conditioning, pooled = compel(prompt)
     depth_image = get_depth_map(image, feature_extractor, depth_estimator)
     generator = torch.manual_seed(33)
     
+    ### TEST aachong
+    image_zoe = zoe(image, detect_resolution=1024, image_resolution=1024)
+    rembg_output = remove(image, alpha_matting=True, alpha_matting_foreground_threshold=200, alpha_matting_background_threshold=200, alpha_matting_erode_size=10)
+    rembg_mask = remove(image, alpha_matting=True, alpha_matting_foreground_threshold=200, alpha_matting_background_threshold=200, alpha_matting_erode_size=10, only_mask=True)
+    final_mask = ImageOps.invert(rembg_mask).point(lambda p: p > 200 and 255)
+    mask_blurred = final_mask.filter(ImageFilter.GaussianBlur(radius=10)).convert('RGB')
+
+    #final_mask = ImageOps.invert(output)
+    #output_image = remove(image)              # Removing the background from the given Image 
+    #img_mask = output_image.convert('L')      # grayscale
+    #mask = ImageOps.invert(img_mask)
+    #final_mask = mask.point(lambda p: p > 240 and 255)
+    #mask_blurred = final_mask.filter(ImageFilter.GaussianBlur(radius=2)).convert('RGB')
+
+    # Debug
+    print(f"Image shape: {np.array(image).shape}")
+    print(f"Image Zoe shape: {np.array(image_zoe).shape}")
+    
     # https://huggingface.co/docs/diffusers/en/api/pipelines/controlnet#diffusers.StableDiffusionControlNetImg2ImgPipeline.__call__
-    generated_images = pipe(
+    generated_image = pipe(
         prompt_embeds=conditioning,
         pooled_prompt_embeds=pooled,
-        negative_prompt=data['negative_prompt'],
-        image=depth_image,
+        negative_prompt=data['negative_prompt']+", window, door, low resolution, banner, logo, watermark, text, deformed, out of focus, surreal, ugly, beginner",
+        image=[image, image_zoe],
+        #mask_image=mask_blurred,
+        #control_image=depth_image,
         num_inference_steps=data['steps'],
         strength=data['strength'],
-        controlnet_conditioning_scale=data['controlnet_conditioning_scale'],
+        #controlnet_conditioning_scale=data['controlnet_conditioning_scale'],
+        controlnet_conditioning_scale=[0.5, 1],
+        control_guidance_end=[0.9, 1],
         generator=generator,
+        width=1024,
+        height=1024
+    ).images[0]
+    
+    generated_image.paste(rembg_output, (0, 0), rembg_output)
+    
+    pipe_inpaint = AutoPipelineForInpainting.from_pipe(pipe, controlnet=None)
+    
+    print(f"Generated Image shape: {np.array(generated_image).shape}")
+    print(f"Mask shape: {np.array(mask_blurred).shape}")
+    
+    generated_images = pipe_inpaint(
+        prompt="shadow, high quality photo of a furniture on the floor, photorealistic",
+        negative_prompt=data['negative_prompt']+", window, door, low resolution, banner, logo, watermark, text, deformed, out of focus, surreal, ugly, beginner",
+        image=generated_image,
+        mask_image=mask_blurred,
+        num_inference_steps=data['steps'],
+        strength=0.5,
+        generator=generator,
+        width=1024,
+        height=1024
     ).images
     
     # create response 
     encoded_images=[]
     for image in generated_images:
-      buffered = BytesIO()
-      image.save(buffered, format="JPEG")
-      encoded_images.append(base64.b64encode(buffered.getvalue()).decode())
+        image.paste(rembg_output, (0, 0), rembg_output)
+        image = image.resize((512,512))
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        encoded_images.append(base64.b64encode(buffered.getvalue()).decode())
     encoded_images.append(depth_image)
 
     # create response
--- src/roomgenerator/model/save_models.py
+++ src/roomgenerator/model/save_models.py
@@ -1,6 +1,6 @@
 import torch
 from transformers import DPTImageProcessor, DPTForDepthEstimation
-from diffusers import StableDiffusionXLControlNetPipeline, ControlNetModel, HeunDiscreteScheduler, AutoencoderKL
+from diffusers import StableDiffusionXLInpaintPipeline, StableDiffusionXLControlNetPipeline, StableDiffusionXLControlNetInpaintPipeline, ControlNetModel, HeunDiscreteScheduler, AutoencoderKL, AutoPipelineForInpainting
 
 # Download and save the models
 depth_estimator = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas")
@@ -9,20 +9,33 @@ depth_estimator.save_pretrained("dpt-hybrid-midas/")
 feature_extractor = DPTImageProcessor.from_pretrained("Intel/dpt-hybrid-midas")
 feature_extractor.save_pretrained("dpt-hybrid-midas/")
 
-controlnet = ControlNetModel.from_pretrained("diffusers/controlnet-depth-sdxl-1.0")
-controlnet.save_pretrained(
-    "controlnet-depth-sdxl-1.0/",
+controlnet1 = ControlNetModel.from_pretrained("destitech/controlnet-inpaint-dreamer-sdxl") #diffusers/controlnet-depth-sdxl-1.0
+controlnet1.save_pretrained(
+    "controlnet-inpaint-dreamer-sdxl/", #controlnet-depth-sdxl-1.0/
     variant="fp16",
-    use_safetensors=True,
     torch_dtype=torch.float16
 )
+
+controlnet2 = ControlNetModel.from_pretrained("diffusers/controlnet-zoe-depth-sdxl-1.0")
+controlnet2.save_pretrained(
+    "controlnet-zoe-depth-sdxl-1.0/",
+    torch_dtype=torch.float16
+)
+
+#controlnet = ControlNetModel.from_pretrained("diffusers/controlnet-depth-sdxl-1.0") # "BertChristiaens/controlnet-seg-room"
+#controlnet.save_pretrained(
+#    "controlnet-depth-sdxl-1.0/",
+#    variant="fp16",
+#    use_safetensors=True,
+#    torch_dtype=torch.float16
+#)
 
 vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
 vae.save_pretrained("sdxl-vae-fp16-fix/")
 
 pipeline = StableDiffusionXLControlNetPipeline.from_pretrained(
-        "stabilityai/stable-diffusion-xl-base-1.0",
-        controlnet=controlnet,
+        "SG161222/RealVisXL_V4.0", 
+        controlnet=[controlnet1, controlnet2],
         vae=vae,
         variant="fp16",
         use_safetensors=True,
@@ -30,4 +43,12 @@ pipeline = StableDiffusionXLControlNetPipeline.from_pretrained(
 )
 
 pipeline.scheduler = HeunDiscreteScheduler.from_config(pipeline.scheduler.config)
-pipeline.save_pretrained("stable-diffusion-xl-base-1.0")
+pipeline.save_pretrained("RealVisXL_V4.0")
+
+#pipeline2 = StableDiffusionXLInpaintPipeline.from_pretrained(
+#        "OzzyGT/RealVisXL_V4.0_inpainting",
+#        torch_dtype=torch.float16,
+#        variant="fp16",
+#        vae=vae,
+#)
+#pipeline2.save_pretrained("RealVisXL_V4.0_inpainting")
